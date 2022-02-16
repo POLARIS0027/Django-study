6d9282154349bfd856edda8b31409e7179d5b1a6
Pola2272 <pola2272@gmail.com>
2022-02-17 05:26:09 +0900

    added Photo app

--- bluebook/project/mysite/urls.py
+++ bluebook/project/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from mysite.views import HomeView 
 
 urlpatterns = [
@@ -23,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
+    path('photo/', include('photo.urls'))
    
     
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
