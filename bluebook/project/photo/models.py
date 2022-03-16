98e92835c65802bf3440bdfebb54770786703038
polaris0027 <pola2272@gmail.com>
2022-03-17 00:40:38 +0900

    꼬임

--- bluebook/project/photo/models.py
+++ bluebook/project/photo/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.urls import reverse
-
+from photo.fields import ThumbnailImageField
 # Create your models here.
 
 class Album(models.Model):
     name = models.CharField(max_length=30)
     description =models.CharField('One line Description', max_length=100, blank=True)
-    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
     
     class Meta:
         ordering=('name',)
@@ -21,9 +20,8 @@ class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField('TITLE', max_length=30)
     description = models.TextField('Photo Description', blank=True)
-    image = models.ImageField('IMAGE', upload_to='SorlPhoto/%Y')
+    image = ThumbnailImageField(upload_to='photo/%Y/%m')
     upload_dt = models.DateTimeField('Upload Date', auto_now_add=True)
-    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True )
     
     class Meta:
         ordering = ('title',)
--- bluebook/project/photo/urls.py
+++ bluebook/project/photo/urls.py
@@ -8,21 +8,5 @@ urlpatterns = [
     path('album/<int:pk>/', views.AlbumLV.as_view(), name='album_list'),
     path('album/<int:pk>/', views.AlbumDV.as_view(), name='album_detail'),
     path('photo/<int:pk>/', views.PhotoDV.as_view(), name='photo_detail'),
-    #photo/album/add/
-    path('album/add/', views.AlbumPhotoCV.as_view(), name='album_add'),
-    #photo/album/change/
-    path('album/change/', views.AlbumChangeLV.as_view(), name='album_change'),
-    #photo/album/99/update/
-    path('album/<int:pk>/update/', views.AlbumPhotoUV.as_view(), name='album_update'),
-    #photo/album/delete/
-    path('album/<int:pk>/delete/', views.AlbumDelV.as_view(), name='album_delete'),
-    #photo/photo/add/
-    path('photo/add/', views.PhotoCV.as_view(), name='photo_add'),
-        #photo/photo/change/
-    path('photo/change/', views.PhotoChangeLV.as_view(), name='photo_change'),
-        #photo/photo/99/update/
-    path('photo/<int:pk>/update/', views.PhotoUV.as_view(), name='photo_update'),
-        #photo/photo/99/delete/
-    path('photo/<int:pk>/delete/', views.PhotoDelV.as_view(), name='photo_delete'),
     
 ]
